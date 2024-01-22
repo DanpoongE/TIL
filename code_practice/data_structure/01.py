ae6ca9de4c0157b4d88fdd1aa5c6b87a7f8c5843
Esther <hwangeunbi0121@gmail.com>
2024-01-22 12:08:00 +0900

    22update

---
+++ code_practice/data_structure/01.py
@@ -0,0 +1,31 @@
+# print(type([1, 2]))
+
+# print(help(str))
+
+# print(help(list)) # append를 확인해 볼 수 있다. 
+
+# def append():
+#     pass
+
+# append() #원래 이렇게 함수 호출
+# #어떤 객체에 속한 함수들은 이런식의 매서드 호출이 가능
+# 리스트.append()
+
+
+# # s.index() 메서드
+# my_str = 'banana'
+# my_str2 = 'dhe21'
+
+# print(my_str.index('w'))
+
+
+#조작 메서드
+# str1 = 'Hello, World!'
+# str2 = 'dhf32'
+
+# new_str1 = str1.replace('World!', 'Python!')
+# print(new_str1) #Hello, Python!
+
+# str3 = "Hello World Good Morning"
+# new_str3 = str3.split()
+# print(new_str3)
---
+++ code_practice/data_structure/02.py
@@ -0,0 +1,19 @@
+# #리스트 값 추가 및 삭제
+# list = [1, 2, 3]
+# set = {4, 5, 6}
+# tuple = (7, 8, 9)
+
+# list.append([4, 5, 6])
+# list.extend(tuple)
+
+# list.insert(3, 'ssafy')
+
+# item = list.pop()
+# print(item)
+# print(list)
+
+#
+my_list = [1, 2, 2, 3, 3, 3]
+num = my_list.count(3)
+
+print(num)
